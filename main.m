8fa5c3d597fe8da7455de8913ae44830f81beaf5
Kaiyu Shi <abcpdo@gmail.com>
2020-11-12 20:11:44 -0600

    plotted self reported scores against GRS

--- main.m
+++ main.m
@@ -25,3 +25,28 @@ for name = {meta(:).filename}
    kinematics(i) = importdata('JIGSAWS/Suturing/kinematics/AllGestures/'+string(name)+'.txt');
    i = i + 1;
 end
+
+%% Visualize something
+N = [];
+I = [];
+E = [];
+for i = 1:num_surgeons
+    if meta(i).skill_self == 'N'
+        N = [N, meta(i).skill_GRS];
+    end
+    if meta(i).skill_self == 'I'
+        I = [I, meta(i).skill_GRS];
+    end
+    if meta(i).skill_self == 'E'
+        E = [E, meta(i).skill_GRS];
+    end
+end
+
+hAxes = axes('NextPlot','add',...           %# Add subsequent plots to the axes,
+             'DataAspectRatio',[1 1 1],...  %#   match the scaling of each axis,
+             'XLim',[0 30],...               %#   set the x axis limit,
+             'YLim',[0 eps],...             %#   set the y axis limit (tiny!),
+             'Color','none');               %#   and don't use a background color
+plot(N,0,'r.',I,0,'g.',E,0,'b.','MarkerSize',10);  %# Plot data set 1
+hold on
+legend('Novice','Intermediate','Expert');
