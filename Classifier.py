4a74595234a386df8cc2295771a3e1004ed15e57
Kaiyu Shi <abcpdo@gmail.com>
2021-01-03 09:31:48 -0800

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -1,8 +1,16 @@
-from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D
+from tensorflow.keras.utils import np_utils
+from tensorflow.keras.datasets import mnist
+import numpy as np
 
-dataset = loadtxt('pima-indians-diabetes.data.txt', delimiter=',')
-X = dataset[:,0:8]
-y = dataset[:,8]
 
+np.random.seed(123)
+# Load pre-shuffled MNIST data into train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+plt.ion()
+plt.imshow(X_train[0])
