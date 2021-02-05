53e73fc57f50524237634f8b023a2bea3d209004
Kaiyu Shi <abcpdo@gmail.com>
2021-02-04 16:55:19 -0800

    added plotting

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import os
+from numpy.core.fromnumeric import mean
+from numpy.lib.polynomial import poly
 from pandas import read_csv
 import torch
 from torch._C import dtype
@@ -10,13 +12,14 @@ import torch.optim as optim
 import torch.autograd as autograd
 from tensorflow.keras.utils import to_categorical
 from tqdm import trange
+import statistics as st
 
 def load_samples(filepath):
 	"""
 		input: path to csv
 		output: list of 2d arrays of shape (sequence, feature)
 	"""
-	Output = [] 
+	Output = []
 	dataframe = read_csv(filepath, header = None)
 	Samples = dataframe.to_numpy()
 	Two_D = np.empty((0,Samples.shape[1]))
@@ -28,7 +31,7 @@ def load_samples(filepath):
 			Output.append(Two_D) #stack on the 2d array
 			Two_D = np.empty((0,Samples.shape[1]))   #empty the 2d array
 
-	return Output 
+	return Output
 
 def create_dataset(name1 = 'ExpertSamples.csv', name2 = 'NoviceSamples.csv'):
 	"""
@@ -76,7 +79,6 @@ def shuffle_and_split(Combined_X,Combined_y,train_ratio,shuffle_index):
 	np.random.shuffle(shuffle_array)
 	Combined_X = Combined_X[shuffle_array]
 	Combined_y = Combined_y[shuffle_array]
-	print(Combined_y)
 
 	#split
 	test_ratio = 1-train_ratio
@@ -104,7 +106,7 @@ class Classifier(nn.Module):
 
 	def forward(self,batch,layers):
 		self.hidden = self.init_hidden(batch.size(0),layers)
-		_, last_hidden = self.lstm(batch,self.hidden)
+		hidden, last_hidden = self.lstm(batch,self.hidden)
 		output = self.dropout(last_hidden[0].squeeze())
 		output = self.fully_connected(output)
 		#output = self.sigmoid(output)
@@ -120,9 +122,12 @@ def train_model(model, train_X, train_y, epochs=20):
 	total_loss = 0
 	torch.autograd.set_detect_anomaly(True)
 
-	for epoch in range(epochs):
+	train_accs = list()
+	test_accs = list()
+
+	for epoch in trange(epochs):
 		#feed each sequence one at a time
-		# for i in trange(train_X.size(0)):  
+		# for i in trange(train_X.size(0)):
 		# 	model.zero_grad()
 		# 	#print(train_X[i].unsqueeze(0).shape)    #inserted dim of 1 to 0th dimension
 		# 	pred = model.forward(train_X[i].unsqueeze(0),1)
@@ -146,57 +151,86 @@ def train_model(model, train_X, train_y, epochs=20):
 		loss = criterion(pred,train_y.float())
 		loss.backward()
 		optimizer.step()
-		print("Epoch {} ".format(epoch) + "loss: {}".format(loss.item()))
+		#print("Epoch {} ".format(epoch) + "loss: {}".format(loss.item()))
 		total_loss += loss.item()
-		
+
 		global test_X
 		global test_y
 
-		acc = model_accuracy(model,train_X,train_y,False)
-		acc = model_accuracy(model,test_X,test_y,True)
+		train_accs.append(model_accuracy(model,train_X,train_y,False))
+		test_accs.append(model_accuracy(model,test_X,test_y,True))
 
-	print("End Of Training")
-	return model
+	#print("End Of Training")
+	return model, train_accs, test_accs
 
 
 def model_accuracy(model, X, y,Test_flag):
 	model.eval()
 	pred = model.forward(X.float(),1)
-	predicted = torch.max(pred,1)[1] 
+	predicted = torch.max(pred,1)[1]
 	actual = torch.max(y.float(),1)[1]
 	correct = ((predicted == actual) == True).sum()
 	total = X.size(0)
 
-	if Test_flag:
-		print('Accuracy of the network on the test set: %d %%' % (100 * correct / total))
-	else:
-		print('Accuracy of the network on the train set: %d %%' % (100 * correct / total))
+	# if Test_flag:
+	# 	print('Accuracy of the network on the test set: %d %%' % (100 * correct / total))
+	# else:
+	# 	print('Accuracy of the network on the train set: %d %%' % (100 * correct / total))
 
 	model.train()
-	return correct/total
+	return (correct/total)*100
 
 
 if __name__ == '__main__':
-	accs = list() #list of accuracies
-	for i in range(1):  #reshuffle each loop
+	epochs = 250
+	reshuffle = 5
+	hidden_dim = 50
+
+	accs = list() #list of final accuracies
+	all_accs = list()
+	for i in trange(reshuffle):  #reshuffle each loop
 		#prepare data
 		Combined_X, Combined_y = create_dataset()
-		train_X,test_X,train_y,test_y = shuffle_and_split(Combined_X, Combined_y,0.7,i+10)  
+		train_X,test_X,train_y,test_y = shuffle_and_split(Combined_X, Combined_y,0.7,i+52)
 
 		#train and evaluate model
-		model = Classifier(train_X.size(2),50,1,2,0) #input dim, hidden dim, num_layers, output dim, dropout ratio
-		model = train_model(model,train_X,train_y,100) # model, X, y, epochs
+		model = Classifier(train_X.size(2),hidden_dim,1,2,0) #input dim, hidden dim, num_layers, output dim, dropout ratio
+		model,train_accs,test_accs = train_model(model,train_X,train_y,epochs) # model, X, y, epochs
 		acc = model_accuracy(model,test_X,test_y,True)
 		accs.append(acc.item())
-
+		all_accs.append([train_accs,test_accs])
+		
 		# #display number of params
 		# model_parameters = filter(lambda p: p.requires_grad, model.parameters())
 		# params = sum([np.prod(p.size()) for p in model_parameters])
 		# print(params)
-	
 	print(accs)
+	print("Mean: {}".format(st.mean(accs)))
+	#print("stddev: {}".format(st.stdev(accs)))
+
+	plt.xlabel("Epochs")
+	plt.ylabel("Accuracy (%)")
+
+	all_accs = np.array(all_accs)
+	for i in range(len(all_accs)):
+		plt.plot(np.transpose(all_accs[i,0]),label='Train', color = (0.5+np.random.random()*0.5,np.random.random()*0.1,np.random.random()*0.1))
+		plt.plot(np.transpose(all_accs[i,1]),label='Test', color = (np.random.random()*0.1,0.5+np.random.random()*0.5,np.random.random()*0.1))
+	handles, labels = plt.gca().get_legend_handles_labels()
+	newLabels, newHandles = [], []
+	for handle, label in zip(handles, labels):
+		if label not in newLabels:
+			newLabels.append(label)
+			newHandles.append(handle)
+	plt.legend(newHandles, newLabels)
+	plt.title('LSTM Expert/Novice Classifier')
+	axis = plt.gca()
+	axis.set_ylim(axis.get_ylim()[::-1])
+	plt.show()
+
 
 	#save trained model
 	__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 	PATH = __location__ + '/LSTM.pth'
 	torch.save(model.state_dict(), PATH)
+	PATH = __location__ + '/fig.png'
+	plt.savefig(PATH,dpi=250)
