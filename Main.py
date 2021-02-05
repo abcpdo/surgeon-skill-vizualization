35d3edffc5797550a9606316d83e27ab8827b95a
Kaiyu Shi <abcpdo@gmail.com>
2021-02-04 16:55:45 -0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,18 @@
+from classifier import *
+from predictor import *
+from dataloader import *
+
+pretrained = False
+
+#Create Dataset 
+DataLoader = DataLoader()
+
+#train classifier network (if not pretrained)
+if(not pretrained):
+    train_network(DataLoader.something)
+
+classification 
+
+#take novice sample
+predict_novice()
+
